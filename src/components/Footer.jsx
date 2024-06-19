24f34f6e26fb467cc08fa1306bed8418ce0f346a
prampokan <prampokan@gmail.com>
2024-06-20 06:42:49 +0700

    responsive navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full flex justify-center items-center py-32 bg-zinc-900">
+    <div className="w-full flex justify-center items-center pt-20 pb-10 bg-zinc-900 px-5 xl:px-0">
         <div className="w-[70rem] flex">
           <div>
             <div className="flex items-center gap-2">
@@ -9,6 +9,10 @@ const Footer = () => {
             </div>
             <p className="text-lg font-bold text-zinc-500 mt-6">Capstone Team bla bla...</p>
             <p className="text-lg font-bold text-zinc-500">© Copyright 2024. All Rights Reserved.</p>
+            <div className="flex gap-5 mt-14">
+              <p className="text-lg font-bold text-zinc-500 cursor-pointer">Github</p>
+              <p className="text-lg font-bold text-zinc-500 cursor-pointer">Linkedin</p>
+            </div>
           </div>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-// import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import Bar from "../elements/Bar"
 
 const Navbar = () => {
-//   const [open, isOpen] = useState(false)
+  const [open, setOpen] = useState(false)
+  const divRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (divRef.current && !divRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="w-full flex justify-center items-center h-16 shadow fixed bg-white z-[99] px-5 xl:px-0">
@@ -11,7 +26,7 @@ const Navbar = () => {
                 <a href="" className="text-xl font-semibold">Meatopia</a>
             </div>
             <div>
-                <ul className="flex gap-5">
+                <ul className="hidden sm:flex gap-5">
                     <li className="text-lg font-medium">
                         <a href="">Home</a>
                     </li>
@@ -25,6 +40,15 @@ const Navbar = () => {
                         <a href="">Contact</a>
                     </li>
                 </ul>
+                <div className="relative flex sm:hidden gap-5" ref={divRef}>
+                    <Bar onclick={() => setOpen(!open)}/>
+                    <div className={`${open ? "opacity-1 translate-y-1" : "invisible"} shadow rounded-xl transition-all absolute w-52 flex flex-col gap-2 top-14 right-0 bg-white p-2 z-10`}>
+                        <a href="" className="bg-white p-2 font-medium">Home</a>
+                        <a href="" className="bg-white p-2 font-medium">About</a>
+                        <a href="" className="bg-white p-2 font-medium">Info</a>
+                        <a href="" className="bg-white p-2 font-medium">Contact</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
